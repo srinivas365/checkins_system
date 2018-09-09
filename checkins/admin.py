11b08549c2e1d0d0c3842d068f9524331bd588df
srinivas365 <m.srinivas365@gmail.com>
2018-09-10 00:02:13 +0530

    initialised authentication

--- checkins/admin.py
+++ checkins/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from .models import Checkins
 # Register your models here.
+
+admin.site.register(Checkins)
--- checkins/models.py
+++ checkins/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+from django.contrib.auth.models import User
+
+class Checkins(models.Model):
+	#latitude,longitude,review,place name
+	latitude=models.FloatField()
+	longitude=models.FloatField()
+	place_name=models.CharField(max_length=500)
+	review=models.CharField(max_length=1000)
